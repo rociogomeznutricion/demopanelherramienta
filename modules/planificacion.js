fb28211f2f77670284bad6400b0713f07be95446
rociogomeznutricion <rociogomeznutricion@gmail.com>
2026-05-23 22:31:21 +0200

    Update planificacion.js

--- modules/planificacion.js
+++ modules/planificacion.js
@@ -28,7 +28,7 @@ window.inicializarPlanificacion = async function() {
         construirInterfazDias(container);
         
         // 2. Simulamos que hacemos "clic" en el Lunes (índice 0) para que no salga la pantalla vacía
-        seleccionarDia(1);
+        seleccionarDia(0);
         
     } catch (e) {
         console.error("Error al cargar planificación:", e);
